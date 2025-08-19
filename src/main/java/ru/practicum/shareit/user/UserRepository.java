0688ce9070da48b94d24877a9634d96416858c5f
Tropuschka <tryan.u@mail.ru>
2025-08-19 20:01:27 +0300

    добавить метод getUserById

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,4 +1,7 @@
 package ru.practicum.shareit.user;
 
+import java.util.Optional;
+
 public interface UserRepository {
+    Optional<User> getUserById(Long id);
 }
--- src/main/java/ru/practicum/shareit/user/UserRepositoryInMemory.java
+++ src/main/java/ru/practicum/shareit/user/UserRepositoryInMemory.java
@@ -3,8 +3,14 @@ package ru.practicum.shareit.user;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 @Repository
 public class UserRepositoryInMemory implements UserRepository {
     HashMap<Long, User> user = new HashMap<>();
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return Optional.ofNullable(user.get(id));
+    }
 }
