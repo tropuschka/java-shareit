631c75b1b58dbeed1f8c39ece26d6c5a7cbacc7d
Tropuschka <tryan.u@mail.ru>
2025-08-19 17:45:15 +0300

    Добавить поля класса ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
 public class ItemDto {
+    private String name;
+    private String description;
+    private boolean available;
+    private Long request;
+
+    public ItemDto(String name, String description, boolean available, Long requestId) {
+        this.name = name;
+        this.description = description;
+        this.available = available;
+        this.request = requestId;
+    }
 }
