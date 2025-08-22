c60d0713f83588fc80d301a6b59f8c0583f7d315
Tropuschka <tryan.u@mail.ru>
2025-08-22 13:09:00 +0300

    добавить интерфейс ItemService

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -1,94 +1,17 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import ru.practicum.shareit.exceptions.ConditionsNotMetException;
-import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.dto.ItemMapper;
-import ru.practicum.shareit.request.ItemRequestRepository;
-import ru.practicum.shareit.user.UserRepository;
 
 import java.util.Collection;
 
-@Service
-public class ItemService {
-    private final ItemRepository itemRepository;
-    private final UserRepository userRepository;
-    private final ItemRequestRepository itemRequestRepository;
+public interface ItemService {
+    Item addItem(Long userId, ItemDto itemDto);
 
-    @Autowired
-    public ItemService(ItemRepository itemRepository,
-                       UserRepository userRepository,
-                       ItemRequestRepository itemRequestRepository) {
-        this.itemRepository = itemRepository;
-        this.userRepository = userRepository;
-        this.itemRequestRepository = itemRequestRepository;
-    }
+    Item updateItem(Long userId, Long itemId, ItemDto itemDto);
 
-    public Item addItem(Long userId, ItemDto itemDto) {
-        checkUser(userId);
-        verifyItemDto(itemDto);
-        Item item = ItemMapper.toItem(itemDto);
-        setItemRequest(itemDto, item);
-        item.setOwner(userId);
-        itemRepository.createItem(item);
-        return item;
-    }
+    Item getItemById(Long itemId);
 
-    public Item updateItem(Long userId, Long itemId, ItemDto itemDto) {
-        checkUser(userId);
-        getItemById(itemId);
-        verifyItemDto(itemDto);
-        Item item = ItemMapper.toItem(itemDto);
-        checkOwner(userId, item);
-        item.setId(itemId);
-        setItemRequest(itemDto, item);
-        itemRepository.updateItem(itemId, item);
-        return item;
-    }
+    Collection<Item> getUserItems(Long userId);
 
-    public Item getItemById(Long itemId) {
-        return itemRepository.getItemById(itemId)
-                .orElseThrow(() -> new NotFoundException("Предмет с ID " + itemId + " не найден"));
-    }
-
-    public Collection<Item> getUserItems(Long userId) {
-        checkUser(userId);
-        return itemRepository.getUserItems(userId);
-    }
-
-    public Collection<Item> searchItem(String query) {
-        checkQuery(query);
-        return itemRepository.searchItem(query);
-    }
-
-    private void verifyItemDto(ItemDto itemDto) {
-        if (itemDto.getName() == null || itemDto.getName().isBlank()) {
-            throw new ConditionsNotMetException("Название предмета не должно быть пустым");
-        }
-    }
-
-    private void checkUser(Long userId) {
-        userRepository.getUserById(userId)
-                .orElseThrow(() -> new NotFoundException("Пользователь с ID " + userId + " не найден"));
-    }
-
-    private void checkOwner(Long userId, Item item) {
-        if (userId != item.getOwner() || item.getOwner() == null) {
-            throw new ConditionsNotMetException("Пользователь с ID " + userId +
-                    " не является владельцем предмета с ID " + item.getId());
-        }
-    }
-
-    private void checkQuery(String query) {
-        if (query.isBlank()) throw new ConditionsNotMetException("Поисковой запрос пуст");
-    }
-
-    private void setItemRequest(ItemDto itemDto, Item item) {
-        if (itemDto.getRequest() != null) {
-            item.setRequest(itemRequestRepository.getRequestById(itemDto.getRequest())
-                    .orElseThrow(() -> new NotFoundException("Запрос с ID " + itemDto.getRequest() + " не найден")));
-        }
-    }
+    Collection<Item> searchItem(String query);
 }
