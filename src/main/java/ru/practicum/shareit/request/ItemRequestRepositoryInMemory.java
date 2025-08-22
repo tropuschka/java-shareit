7c5293690906eb64247be0bf1ba4e3019afe18cc
Tropuschka <tryan.u@mail.ru>
2025-08-22 13:37:22 +0300

    bugfix

--- src/main/java/ru/practicum/shareit/request/ItemRequestRepositoryInMemory.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestRepositoryInMemory.java
@@ -1,8 +1,11 @@
 package ru.practicum.shareit.request;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.HashMap;
 import java.util.Optional;
 
+@Repository
 public class ItemRequestRepositoryInMemory implements ItemRequestRepository {
     private final HashMap<Long, ItemRequest> requests = new HashMap<>();
 
