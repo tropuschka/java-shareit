0eb03a52f1ebfa2010df36d58072670db5878ea3
Tropuschka <tryan.u@mail.ru>
2025-09-05 19:15:57 +0300

    добавить аннотации JPA

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -1,14 +1,25 @@
 package ru.practicum.shareit.item;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+@Entity
+@Table(name = "Item")
 @Data
 @RequiredArgsConstructor
 public class Item {
+    @Id
     private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "description", nullable = false)
     private String description;
+    @Column(name = "owner_id", nullable = false)
     private Long owner;
+    @Column(name = "available", nullable = false)
     private Boolean available;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,12 +1,21 @@
 package ru.practicum.shareit.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+@Entity
+@Table(name = "User")
 @Data
 @RequiredArgsConstructor
 public class User {
+    @Id
     private Long id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "email", nullable = false)
     private String email;
 }
